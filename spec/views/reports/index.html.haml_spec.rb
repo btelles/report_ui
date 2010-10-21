920c35f8a62ce0baefb4bf601b318ce1b920d367
Bernie Telles <bernardo.telles@dms.myflorida.com>
2010-10-21 09:01:42 -0400

    test index 'no canned reports'

--- spec/views/reports/index.html.haml_spec.rb
+++ spec/views/reports/index.html.haml_spec.rb
@@ -6,10 +6,16 @@ def mock_report(stubs = {})
                       :description => 'aoeu'}.merge(stubs))
 end
 describe "reports#index" do
-  it "Shows a list of given controllers " do
+  it "Shows a list of given reports" do
     view.should_receive(:reports).at_least(1).times.and_return([mock_report])
     view.stub(:custom_reports => [])
     render :template => 'reports/index'
     rendered.should have_selector("td:contains('some report')")
+  end
+  it "if no reports are present, it tells the user so" do
+    view.should_receive(:reports).at_least(1).times.and_return([])
+    view.stub(:custom_reports => [])
+    render :template => 'reports/index'
+    rendered.should contain("no canned")
   end
 end
