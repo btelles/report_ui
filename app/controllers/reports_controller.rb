ea1666debd883c3df4cc1db61ff69e72c4da36ce
Bernie Telles <bernardo.telles@dms.myflorida.com>
2010-10-22 10:41:14 -0400

    fix autotesting

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,8 @@
 class ReportsController < ApplicationController
   expose :report
+
   expose(:reports) { Report.all }
+
   expose(:custom_reports) do
     if respond_to?(:current_user) && current_user.respond_to?(:reports)
       current_user.reports
@@ -9,7 +11,5 @@ class ReportsController < ApplicationController
     end
   end
 
-  def columns
-    @available_columns = report.available_columns
-  end
+  expose(:available_columns) { report.available_columns }
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,13 @@
 # from the project root directory.
 ENV["RAILS_ENV"] ||= 'test'
 ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '/../../../..'
-require 'ruby-debug'
+#require 'ruby-debug'
 
 require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config/environment'))
 
 def load_schema
   config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
-  ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + '/debug.log')
+  #ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + '/debug.log')
   db_adapter = 'sqlite3'
   ActiveRecord::Base.establish_connection(config[db_adapter])
   load(File.dirname(__FILE__) + '/schema.rb')
