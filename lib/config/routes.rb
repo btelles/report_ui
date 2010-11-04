ef8d3ca42122531e9d52a3f954643ca535532a1f
Bernie Telles <bernardo.telles@dms.myflorida.com>
2010-11-04 17:16:58 -0400

    bogus

---
+++ lib/config/routes.rb
@@ -0,0 +1,7 @@
+ Rails.application.routes.draw do |map|
+   #reports
+   resources :reports do
+     get :columns
+   end
+ end
+
--- lib/reporter.rb
+++ lib/reporter.rb
@@ -2,9 +2,9 @@
 module Reporter
   mattr_accessor :reportable_models
   @@reportable_models = []
+  require 'reporter/engine' if defined?(Rails)
 end
 require 'orm_adapter'
 require 'reporter/routing'
 require 'reporter/schema'
 require 'reporter/orm/active_record'
-require 'reporter/engine' if defined?(Rails)
