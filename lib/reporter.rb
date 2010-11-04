cd12ea7b6d56007625bd5fcf04c389fcd1189361
Bernie Telles <bernardo.telles@dms.myflorida.com>
2010-11-04 17:44:38 -0400

    make sure both tests and app version work

--- lib/reporter.rb
+++ lib/reporter.rb
@@ -1,10 +1,9 @@
-
+require 'orm_adapter'
+require 'reporter/routing'
+require 'reporter/schema'
+require 'reporter/orm/active_record'
 module Reporter
   mattr_accessor :reportable_models
   @@reportable_models = []
   require 'reporter/engine' if defined?(Rails)
 end
-require 'orm_adapter'
-require 'reporter/routing'
-require 'reporter/schema'
-require 'reporter/orm/active_record'
